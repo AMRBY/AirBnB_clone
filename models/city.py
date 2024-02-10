ad7f4e12d753882f84aacf067c61936103c6ca48
putbullet <soulaimanettabaas@gmail.com>
2024-02-10 15:53:28 -0800

    more classes

---
+++ models/city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+City
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    Represent a city.
+
+    Attributes:
+        state_id (str): The state id.
+        name (str): The name of the city.
+    """
+
+    state_id = ""
+    name = ""
