f71c8c929ba4267eb79b84188cae61f332f527b4
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-02-11 18:06:55 +0000

    mandatory

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,57 @@
-#!/usr/bin/env python3
-"""
-BaseModel is a base class of the hbnb project
-"""
-from uuid import uuid4
+#!/usr/bin/python3
+
+import uuid
 from datetime import datetime
+
+"""
+till we finish the class
+"""
 
 
 class BaseModel:
-    """BaseModel defines all common attributes/methods for other classes
-        """
+
+    """define the attributes and methods"""
     def __init__(self, *args, **kwargs):
-        """__init__ function to initiate attributes
-        Attributes:
-            id: UUID
-            created_at: date and time of creation
-            updated_at: date and time of updating
-        """
         from models import storage
-        if not kwargs:
-            self.id = str(uuid4())
+        """
+        """
+        if kwargs:
+            if "__class__" in kwargs:
+                del kwargs["__class__"]
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key, datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if (key == "created_at" or key == "updated_at"):
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
-
-    def __str__(self):
-        """ str return a string representation of an object"""
-        return str("[{}] ({}) <{}>"
-                   .format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        """ save update the attribute <update_at>"""
         from models import storage
+        """updates the public instance attribute,
+        updated_at with the current datetime
+        """
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """ to_dict return a dictionnary"""
-        new_dict = self.__dict__.copy()
-        new_dict["created_at"] = datetime.isoformat(self.created_at)
-        new_dict["updated_at"] = datetime.isoformat(self.updated_at)
-        new_dict["__class__"] = self.__class__.__name__
-        return (new_dict)
+        """Convert the object to a dictionary representation.
+        Returns:
+            dict: A dictionary containing the object's attributes,
+            class name, and ISO-formatted date strings.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = obj_dict['created_at'].isoformat()
+        obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
+
+        return obj_dict
+
+    def __str__(self):
+        """Custom string representation of the object"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
