b4bec5c55ac2bbf73cb5b8570342a09c1e851d4b
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-02-11 18:20:06 +0000

    mandatory

--- models/base_model.py
+++ models/base_model.py
@@ -1,79 +1,49 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-Custom base class for the entire project
+BaseModel is a base class of the hbnb project
 """
-
 from uuid import uuid4
 from datetime import datetime
-import models
 
 
 class BaseModel:
-    """Custom base for all the classes in the AirBnb console project
-
-    Arttributes:
-        id(str): handles unique user identity
-        created_at: assigns current datetime
-        updated_at: updates current datetime
-
-    Methods:
-        __str__: prints the class name, id, and creates dictionary
-        representations of the input values
-        save(self): updates instance arttributes with current datetime
-        to_dict(self): returns the dictionary values of the instance obj
-
-    """
-
-    def __init__(self, *args, **kwargs):
-        """Public instance artributes initialization
-        after creation
-
-        Args:
-            *args(args): arguments
-            **kwargs(dict): attrubute values
-
+    """BaseModel defines all common attributes/methods for other classes
         """
-        DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
-        if not kwargs:
+    def __init__(self, *args, **kwargs):
+        """__init__ function to initiate attributes
+        Attributes:
+            id: UUID
+            created_at: date and time of creation
+            updated_at: date and time of updating
+        """
+        from models import storage
+        if (kwargs == {}):
             self.id = str(uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-            models.storage.new(self)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
         else:
             for key, value in kwargs.items():
-                if key in ("updated_at", "created_at"):
-                    self.__dict__[key] = datetime.strptime(
-                        value, DATE_TIME_FORMAT)
-                elif key[0] == "id":
-                    self.__dict__[key] = str(value)
-                else:
-                    self.__dict__[key] = value
+                if (key == "created_at" or key == "updated_at"):
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
-        """
-        Returns string representation of the class
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        """ str return a string representation of an object"""
+        return str("[{}] ({}) <{}>"
+                   .format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        """
-        Updates the public instance attribute:
-        'updated_at' - with the current datetime
-        """
-        self.updated_at = datetime.utcnow()
-        models.storage.save()
+        """ save update the attribute <update_at>"""
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        """
-        Method returns a dictionary containing all
-        keys/values of __dict__ instance
-        """
-        map_objects = {}
-        for key, value in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                map_objects[key] = value.isoformat()
-            else:
-                map_objects[key] = value
-        map_objects["__class__"] = self.__class__.__name__
-        return map_objects
+        """ to_dict return a dictionnary"""
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = datetime.isoformat(self.created_at)
+        new_dict["updated_at"] = datetime.isoformat(self.updated_at)
+        new_dict["__class__"] = self.__class__.__name__
+        return (new_dict)
