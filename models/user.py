4d8f6d1a4e78d0421356ef0160f2150a20450aa4
putbullet <soulaimanettabaas@gmail.com>
2024-02-12 08:35:51 -0800

    user

--- models/user.py
+++ models/user.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-""" Class user that inherits from base model"""
-
+"""Defines the User class."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ Class user that inherits from base model """
+    """Represent a User.
+
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+
     email = ""
     password = ""
     first_name = ""
