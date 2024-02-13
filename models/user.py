5ed411758070764a914743305ff498d8a921e5cf
putbullet <soulaimanettabaas@gmail.com>
2024-02-13 10:09:06 -0800

    i updated user and destroy an sdded some dunctionsto the console

--- models/user.py
+++ models/user.py
@@ -17,3 +17,7 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize User instance."""
+        super().__init__(*args, **kwargs)
